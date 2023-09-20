bd5606895c7bf3b44f673c91d54dd14778e94bb7
UGEcko <vanhelsing314@outlook.com>
2023-09-19 20:11:45 -0400

    Updated!
    Added helpful-ish comments for better understanding of morph

--- ReMapper/Other/Morph.ts
+++ ReMapper/Other/Morph.ts
@@ -40,17 +40,17 @@ export function morphEnvNote(startEnd: Vec2, NoteEnvTrack: string[], filePath: s
     const end = startEnd[1]
     const noteTrack = NoteEnvTrack[0]
     const envTrack = NoteEnvTrack[1]
-    const envPos = filterAndExtractRandom(filePath, envTrack)
+    const envPos = filterAndExtractRandom(filePath, envTrack) // Goated func
     const xyzPos:Vec3 = envPos?.pos
     const xyzRot:Vec3 = envPos?.rot
     const xyzScale:Vec3 = envPos?.scale
 
-    const position = arrMul(xyzPos, 1)
-    const localRotation = arrMul(xyzRot, 1)
-    const scale = arrMul(xyzScale, 5)
+    const position = arrMul(xyzPos, 1) // Multiply position of geo cube
+    const localRotation = arrMul(xyzRot, 1) // Multiply rotation of geo cube
+    const scale = arrMul(xyzScale, 5) // Multiply scale of geo cube
     const a = new Geometry("Cube")
-    const scaleOff = calculateScaleOffset(xyzScale, scale, "Y")
-    a.position = [position[0], position[1]+scaleOff/2, position[2]]
+    const scaleOff = calculateScaleOffset(xyzScale, scale, "Y") // Change "Y" to other axis for diff. scaling
+    a.position = [position[0], position[1]+scaleOff/2, position[2]] // Idk how I figured out the "equation" for stacking different-scaled objects on top of each other so quickly
     a.localRotation = localRotation
     a.scale = scale
     a.push()
